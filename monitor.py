f1e549be4c355dbf81ce404f5b181e420318bf8b
Daniel Madden <dmadden@dyn.com>
2015-10-15 13:40:48 -0400

    Initial commit

---
+++ monitor.py
@@ -0,0 +1,56 @@
+__author__ = 'dmadden'
+import os
+import urllib2
+import ConfigParser
+import smtplib
+from email.mime.text import MIMEText
+
+config = ConfigParser.RawConfigParser()
+config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ip_config.cfg'))
+
+
+def get_ip_config():
+    try:
+        return config.get("info", "ip")
+    except:
+        return None
+
+def get_email_password():
+    try:
+        return config.get('info', 'password')
+    except:
+        return None
+
+def ip_get(url):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    html = opener.open(url)
+    return html.read()
+
+def send_email(ip):
+
+    sending_email = "dynamicipalert@gmail.com"
+    recieving_email = "guthran@gmail.com"
+    subject = "IP ADDRESS CHANGED!"
+    body = "The ip address for schroedinger has changed. Please update your DNS records to reflect the new IP: {}".format(ip)
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = sending_email
+    message['To'] = recieving_email
+    s = smtplib.SMTP('smtp.gmail.com', port=587)
+    s.ehlo()
+    s.starttls()
+    s.login(sending_email, get_email_password())
+    s.sendmail(sending_email, [recieving_email], message.as_string())
+    s.quit()
+
+
+def check_ip():
+    ip = get_ip_config()
+    new_ip = ip_get("http://ipv4bot.whatismyipaddress.com")
+    if ip != new_ip:
+        send_email(new_ip)
+
+
+if __name__ == "__main__":
+    check_ip()
